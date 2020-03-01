9c886b4d65b43da2878a55dfeec34dd26caf0359
DanielFryy <dgz1606@gmail.com>
2020-03-01 12:27:10 -0500

    More classes and custom types

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
+// Project Type
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {}
+}
+
 // Project State Management
+type Listener = (item: Project[]) => void;
+
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {}
@@ -14,17 +26,12 @@ class ProjectState {
     return ProjectState.instance;
   };
 
-  addListener = (listenerFn: Function) => {
+  addListener = (listenerFn: Listener) => {
     this.listeners.push(listenerFn);
   };
 
   addProject = (title: string, description: string, numOfPeople: number) => {
-    const newProject = {
-      id: Math.random().toString(),
-      title,
-      description,
-      people: numOfPeople,
-    };
+    const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
     this.projects.push(newProject);
     for (const listenerFn of this.listeners) {
       listenerFn([...this.projects]);
@@ -82,7 +89,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!;
@@ -93,7 +100,7 @@ class ProjectList {
     this.element = <HTMLElement>importedNode.firstElementChild;
     this.element.id = `${type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
     });
