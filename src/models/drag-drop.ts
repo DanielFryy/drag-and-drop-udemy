7e00a4dbb42bf5873f3c6f076db9d3bedd35ae4d
DanielFryy <dgz1606@gmail.com>
2020-03-01 17:04:18 -0500

    Using ES Modules

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,11 @@
+// Drag & Dop Interface
+export interface Draggable {
+  dragStartHandler: (event: DragEvent) => void;
+  dragEndHandler: (event: DragEvent) => void;
+}
+
+export interface DragTarget {
+  dragOverHandler: (event: DragEvent) => void;
+  dragHandler: (event: DragEvent) => void;
+  dragLeaveHandler: (event: DragEvent) => void;
+}
